44b3c863587e6b92114c06e88593890583097e75
Dinesh A <102590808+D1nesh12@users.noreply.github.com>
2024-03-21 21:42:26 +0530

    Add files via upload

---
+++ search.c
@@ -0,0 +1,27 @@
+/*
+Name: Dinesh A
+Date:
+Description:
+Input:
+Output:
+*/
+
+#include "inverted.h"
+
+node *search(node *head, char *word)
+{
+    if(head == NULL)
+	return NULL;
+    //over all word index
+    while(head != NULL)
+    {
+	if(strcasecmp(head->word,word) == 0)
+	    return head;
+
+	head = head->link;
+    }
+    return NULL;
+
+}
+
+
